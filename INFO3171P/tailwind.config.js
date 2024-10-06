fa4402ad1f483580e0f64ff9711d199520f9e53f
itsLeslie69 <itsLeslie69@users.noreply.github.com>
2024-10-05 21:30:00 -0500

    UI Fixes, Typography and Offers

--- INFO3171P/tailwind.config.js
+++ INFO3171P/tailwind.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   content: [
     '*.{html,js}',
-    './pages/**/*.{html,js',
-    './components/**/*.{html,js}',
+    //'./pages/**/*. {html, js}',
+    './pages/**/*.html', // ??? why does this work?
+    './components/**/*. {html, js}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
